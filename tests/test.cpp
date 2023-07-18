d6bf4dea4057f12112d1e62045ffd686e129b8d7
Kelbon Nik <kelbonage@gmail.com>
2023-07-19 02:24:11 +0400

    parse or exit

--- tests/test.cpp
+++ tests/test.cpp
@@ -10,13 +10,13 @@ int main(int argc, char* argv[]) {
   cli::error_code ec;
   o = cli::parse(argc, argv, ec);
   if (ec)
-    return (std::cout << -2), -2;
+    return -2;
   if (!o.mytag)
-    return (std::cout << -3), -3;
+    return -3;
   if (o.hello_world != "ITS HELLO WORLD")
-    return (std::cout << -4), -4;
+    return -4;
   if (!o.works)
-    return (std::cout << -5), -5;
+    return -5;
   std::cout << cli::program_name() << '\n';
   for (auto arg : cli::program_args())
     std::cout << arg << '\n';
